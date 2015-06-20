04efa2677c9ef3d991a42ff79ea9a45612882dde
sssh9300662 <vita73917@gmail.com>
2015-06-20 14:02:53 +0800

    Add statement

--- src.main.java/design/behavioral/strategy/Main.java
+++ src.main.java/design/behavioral/strategy/Main.java
@@ -3,7 +3,7 @@ package design.behavioral.strategy;
 public class Main {
 
     public static void main(String args[]) {
-
+        //assign business logic to individual components
         AttackMode attackMode = new GunAttack();
         SpeedDown speedDown = new GassSpeedDown();
         SpeedUp speedUp = new GassSpeedUp();
